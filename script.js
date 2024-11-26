e95f2da598d6253044d7e501b619b349b4ea42d1
Stefanospedicato <stefano.spedicato11@gmail.com>
2024-11-26 15:15:00 +0100

    feat: add player's numbers in array

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function randomNumber(){
   for(let i = 0; i < 5; i++){
   let randomNumbers = Math.ceil(Math.random()*50)
   arrayNumeriRandom.push(randomNumbers)
-}
+  }
 }
 
 randomNumber();
@@ -18,10 +18,38 @@ let listItems = ''
 for(let i= 0; i<arrayNumeriRandom.length; i++){
   listItems += `<li>${arrayNumeriRandom[i]}</li>`
 }
-
 lista.innerHTML = listItems
 
-//Pusho i numeri scelti dal giocatore nell'array vuoto
-const numeriGiocatore = document.querySelector('.numero-scelto');
-const gruppoInput = document.getElementById('inputs');
 
+//Aziono conto alla rovescia
+const countdown = document.getElementById('countdown');
+let seconds = 3;
+countdown.innerHTML=seconds--
+const risposte = document.getElementById('risposte')
+const button = document.getElementById('bottone')
+const intervalId = setInterval(()=>{ countdown.innerHTML = seconds;
+  if(seconds === 0){
+    clearInterval(intervalId)
+    lista.classList.add('d-none');
+    risposte.classList.remove('d-none');
+    button.disabled = false;
+  }
+seconds--
+}, 1000)
+
+
+//Pusho i numeri scelti dal giocatore nell'array vuoto
+const numeroGiocatore1 = document.querySelector('.numero-scelto1');
+const numeroGiocatore2 = document.querySelector('.numero-scelto2');
+const numeroGiocatore3 = document.querySelector('.numero-scelto3');
+const numeroGiocatore4 = document.querySelector('.numero-scelto4');
+const numeroGiocatore5 = document.querySelector('.numero-scelto5');
+
+button.addEventListener('click', ()=>{
+  arrayNumeriGiocatore.push(numeroGiocatore1.value)
+  arrayNumeriGiocatore.push(numeroGiocatore2.value)
+  arrayNumeriGiocatore.push(numeroGiocatore3.value)
+  arrayNumeriGiocatore.push(numeroGiocatore4.value)
+  arrayNumeriGiocatore.push(numeroGiocatore5.value)
+  console.log(arrayNumeriGiocatore)
+})
